881eceac6f11b02499e6f6e9621789db7e8d9855
FilterAsync <eminem39393@yahoo.com>
2022-06-28 20:28:15 +0700

    Added matrix controller as a component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import { useState } from 'react';
-import {
-	removeAllSpaces,
-	strToCoord,
-	isValidMatrixStr,
-	getRandomEntry,
-	matrixToStr,
-	arrToCoordStr,
-} from './util';
+import { removeAllSpaces, strToCoord, isValidMatrixStr, getRandomEntry } from './util';
 import BFS from './bfs';
 import Matrix from './components/Matrix';
-import InputField from './components/InputField';
+import Controller from './components/Controller';
 import matrices from './matrices.json';
 
 const randomMatrix = getRandomEntry(matrices);
@@ -89,49 +82,10 @@ function App() {
 				</div>
 				<div>
 					<h2>Matrix Controller</h2>
-					<form onSubmit={onSubmit}>
-						<InputField
-							name="matrix"
-							label="Matrix:"
-							textarea={true}
-							defaultValue={matrixToStr(matrix)}
-							onChange={onMatrixChanged}
-						>
-							<p>
-								Put <code>0</code> for obstacle, and <code>1</code> for path.
-							</p>
-							<p>
-								<b>Note:</b> Path movements are up, down, left, right.
-							</p>
-						</InputField>
-						<InputField
-							name="source"
-							label="Source:"
-							type="text"
-							defaultValue={arrToCoordStr(source)}
-							onChange={onSourceChanged}
-						/>
-						<div>
-							<label htmlFor="destination">Destination:</label>
-							<input
-								type="text"
-								name="destination"
-								defaultValue={arrToCoordStr(dest)}
-								onChange={onDestChanged}
-							/>
-						</div>
-						<p>
-							Coordinates must be in the form (<i>x</i>, <i>y</i>).
-						</p>
-						<p hidden={!error} className="error-message">
-							{error}
-						</p>
-						<div className="button-list-row">
-							<button type="submit" disabled={!!error}>
-								Perform BFS
-							</button>
-						</div>
-					</form>
+					{Controller(
+						{ matrix, source, dest, error },
+						{ onSubmit, onMatrixChanged, onSourceChanged, onDestChanged },
+					)()}
 				</div>
 			</div>
 			<hr />
