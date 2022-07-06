1c2b4b55eacdc96156045d718d19ba525dfadbf2
FilterAsync <eminem39393@yahoo.com>
2022-07-06 16:59:03 +0700

    Split components into `MatrixRow` & `MatrixCol`

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,36 +1,28 @@
 import React, { useContext } from 'react';
 import MatrixContext from '../MatrixContext';
 import { CreateUUID } from '../util';
+import MatrixCol from './MatrixCol';
+import MatrixRow from './MatrixRow';
 
 const Matrix = React.memo(
 	({ error, ...props }) => {
 		const { matrix, source, dest } = useContext(MatrixContext);
 		return (
 			<div className="matrix" {...props}>
-				{matrix.map((_row, i1) => {
-					const row = (
-						<div key={`row-${i1}`} className="matrix-row">
-							{_row.map((col, i2) => {
-								const role = col === 1 ? 'path' : 'obstacle';
-								const isSource = i1 === source[0] && i2 === source[1] ? 'source' : '';
-								const isDes = i1 === dest[0] && i2 === dest[1] ? 'destination' : '';
-								return (
-									<span
-										key={CreateUUID() + `:${i1}${i2}`}
-										id={col === 1 ? `${i1},${i2}` : undefined}
-										className={'matrix-col ' + role + ' ' + (isSource || isDes)}
-									>
-										<div className="col-content">
-											{i1}
-											<sub>{i2}</sub>
-										</div>
-									</span>
-								);
-							})}
-						</div>
-					);
-					return row;
-				})}
+				{matrix.map((row, i1) => (
+					<MatrixRow key={'row-' + i1}>
+						{row.map((col, i2) => (
+							<MatrixCol
+								key={'col-' + CreateUUID() + `:${i1}${i2}`}
+								value={col}
+								row={i1}
+								col={i2}
+								source={source}
+								dest={dest}
+							/>
+						))}
+					</MatrixRow>
+				))}
 			</div>
 		);
 	},
