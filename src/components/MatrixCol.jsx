b5f03da2ae845aadd516114fae2ffc89c4e796d5
FilterAsync <eminem39393@yahoo.com>
2022-07-10 15:25:35 +0700

    Weighted matrix: display cost of column

--- src/components/MatrixCol.jsx
+++ src/components/MatrixCol.jsx
@@ -1,5 +1,5 @@
 function MatrixCol({ value, row, col, source, dest, children, ...props }) {
-	let role = value === 1 ? 'path' : 'obstacle';
+	let role = value === 0 ? 'obstacle' : 'path';
 
 	const isSource = row === source[0] && col === source[1];
 	const isDest = row === dest[0] && col === dest[1];
@@ -12,10 +12,21 @@ function MatrixCol({ value, row, col, source, dest, children, ...props }) {
 		role += 'destination';
 	}
 
+	let cost;
+
+	if (value !== 0 && value !== 1) {
+		cost = <div className="col-cost">{value}</div>;
+	}
+
 	return (
-		<div className={'matrix-col ' + role} id={value === 1 ? `${row},${col}` : undefined} {...props}>
+		<div
+			className={'matrix-col ' + role}
+			id={value === 1 ? `${row},${col}` : undefined}
+			{...props}
+		>
 			{row}
 			<sub>{col}</sub>
+			{cost}
 		</div>
 	);
 }
