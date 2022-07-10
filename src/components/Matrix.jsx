bc9e35fc077506f6cdd9278a7d8f6fc2a1117d39
FilterAsync <eminem39393@yahoo.com>
2022-07-10 15:22:25 +0700

    Forwarding ref

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -5,28 +5,30 @@ import MatrixCol from './MatrixCol';
 import MatrixRow from './MatrixRow';
 
 const Matrix = React.memo(
-	({ error, ...props }) => {
-		const { matrix, source, dest } = useContext(MatrixContext);
-		return (
-			<div id="matrix" {...props}>
-				{matrix.map((row, i1) => (
-					<MatrixRow key={'row-' + i1}>
-						{row.map((col, i2) => (
-							<MatrixCol
-								key={'col-' + CreateUUID() + `:${i1}${i2}`}
-								value={col}
-								row={i1}
-								col={i2}
-								source={source}
-								dest={dest}
-							/>
-						))}
-					</MatrixRow>
-				))}
-			</div>
-		);
-	},
-	(_prevProps, nextProps) => !!nextProps.error,
+	React.forwardRef(
+		({ error, ...props }, ref) => {
+			const { matrix, source, dest } = useContext(MatrixContext);
+			return (
+				<div id="matrix" {...props} ref={ref}>
+					{matrix.map((row, i1) => (
+						<MatrixRow key={'row-' + i1}>
+							{row.map((col, i2) => (
+								<MatrixCol
+									key={'col-' + CreateUUID() + `:${i1}${i2}`}
+									value={col}
+									row={i1}
+									col={i2}
+									source={source}
+									dest={dest}
+								/>
+							))}
+						</MatrixRow>
+					))}
+				</div>
+			);
+		},
+		(_prevProps, nextProps) => !!nextProps.error,
+	),
 );
 
 export default Matrix;
