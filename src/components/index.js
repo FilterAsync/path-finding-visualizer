3315e5e400d5b50b9a084ae439f68f07e33c36fc
FilterAsync <eminem39393@yahoo.com>
2022-07-11 13:00:53 +0700

    Aggregate modules

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+export { default as Matrix } from './Matrix';
+
+export { default as Controller } from './Controller';
+
+export { default as MatrixCol } from './MatrixCol';
+
+export { default as MatrixRow } from './MatrixRow';
+
+export { default as FlexContainer } from './FlexContainer';
+
+export { default as ButtonListRow } from './ButtonListRow';
+
+export { InputField, DropdownField, CheckboxField } from './InputField';
