3d4c560a381bef756d7b64b3823af5dcd62d0b05
FilterAsync <eminem39393@yahoo.com>
2022-07-11 13:01:52 +0700

    ESLint: component display name

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,34 +1,31 @@
 import React, { useContext } from 'react';
 import MatrixContext from '../MatrixContext';
 import { CreateUUID } from '../util';
-import MatrixCol from './MatrixCol';
-import MatrixRow from './MatrixRow';
+import { MatrixCol, MatrixRow } from './components';
 
-const Matrix = React.memo(
-	React.forwardRef(
-		({ error, ...props }, ref) => {
-			const { matrix, source, dest } = useContext(MatrixContext);
-			return (
-				<div id="matrix" {...props} ref={ref}>
-					{matrix.map((row, i1) => (
-						<MatrixRow key={'row-' + i1}>
-							{row.map((col, i2) => (
-								<MatrixCol
-									key={'col-' + CreateUUID() + `:${i1}${i2}`}
-									value={col}
-									row={i1}
-									col={i2}
-									source={source}
-									dest={dest}
-								/>
-							))}
-						</MatrixRow>
+// eslint-disable-next-line no-unused-vars
+const Matrix = React.forwardRef(function Matrix({ error, ...props }, ref) {
+	const { matrix, source, dest } = useContext(MatrixContext);
+	return (
+		<div id="matrix" {...props} ref={ref}>
+			{matrix.map((row, i1) => (
+				<MatrixRow key={'row-' + i1}>
+					{row.map((col, i2) => (
+						<MatrixCol
+							key={'col-' + CreateUUID() + `:${i1}${i2}`}
+							value={col}
+							row={i1}
+							col={i2}
+							source={source}
+							dest={dest}
+						/>
 					))}
-				</div>
-			);
-		},
-		(_prevProps, nextProps) => !!nextProps.error,
-	),
-);
+				</MatrixRow>
+			))}
+		</div>
+	);
+});
 
-export default Matrix;
+const areEqual = (_prevProps, nextProps) => !!nextProps.error;
+
+export default React.memo(Matrix, areEqual);
